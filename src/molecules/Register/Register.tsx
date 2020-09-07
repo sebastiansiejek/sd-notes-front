d63e868fed52dcd4df2fdc1d0f4d7d148775ba5d
Sebastian Siejek <contact@sebastiansiejek.dev>
2020-09-07 18:27:03 +0200

    feat: add register

---
+++ src/molecules/Register/Register.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import InputPassword from 'atoms/InputPassword'
+import { Form, Input, Button } from 'antd'
+import { SmileTwoTone, MailTwoTone } from '@ant-design/icons'
+
+export default () => (
+  <>
+    <Form>
+      <Form.Item name='name' rules={[{ required: true, message: 'Please input your username!' }]}>
+        <Input placeholder='name' prefix={<SmileTwoTone />} />
+      </Form.Item>
+      <Form.Item name='email' rules={[{ required: true, message: 'Please input your email!' }]}>
+        <Input placeholder='email' type='email' prefix={<MailTwoTone />} />
+      </Form.Item>
+      <Form.Item name='password' rules={[{ required: true, message: 'Please input your password!' }]}>
+        <InputPassword />
+      </Form.Item>
+      <Button type='primary' htmlType='submit'>
+        Sign In
+      </Button>
+    </Form>
+  </>
+)
