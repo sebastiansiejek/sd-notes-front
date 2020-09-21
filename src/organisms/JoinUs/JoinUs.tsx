ec4f6f60d87cc347e2e15f8c24fee801b5a7d637
Sebastian Siejek <contact@sebastiansiejek.dev>
2020-09-21 20:16:40 +0200

    feat: add JoinUs

--- src/organisms/JoinUs/JoinUs.tsx
+++ src/organisms/JoinUs/JoinUs.tsx
@@ -1,10 +1,43 @@
-import React from 'react'
-import Register from 'molecules/Register'
 import Login from 'molecules/Login'
+import React, { useState } from 'react'
+import Register from 'molecules/Register'
+import styled from 'styled-components'
+import { Card, Radio } from 'antd'
 
-export default () => (
-  <>
-    <Login />
-    <Register />
-  </>
-)
+const Container = styled.div`
+  max-width: 440px;
+  margin: 30rem auto;
+`
+
+const ButtonsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
+export default () => {
+  const [isVisible, setVisible] = useState('login')
+
+  const handleVisible = (e: any) => {
+    setVisible(e.target.value)
+  }
+
+  return (
+    <Container>
+      <Card
+        title={
+          <ButtonsContainer>
+            <Radio.Group onChange={(e) => handleVisible(e)}>
+              <Radio.Button value='login' checked={isVisible === 'login' ? true : false}>
+                Sign in
+              </Radio.Button>
+              <Radio.Button value='register'>Register</Radio.Button>
+            </Radio.Group>
+          </ButtonsContainer>
+        }
+      >
+        {isVisible === 'login' && <Login />}
+        {isVisible === 'register' && <Register />}
+      </Card>
+    </Container>
+  )
+}
