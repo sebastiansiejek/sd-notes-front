6cc92f3e3f8657df9bf8711eae5b6e1caf7ee129
Sebastian Siejek <contact@sebastiansiejek.dev>
2020-09-24 22:07:16 +0200

    feat: add redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
 import React from 'react'
+import store from 'store'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import { routes } from 'routes/configRoutes'
 
 function App() {
   return (
-    <BrowserRouter>
-      <div style={{ width: '100%' }}>
-        <Switch>
-          {routes.map((route) => (
-            <Route
-              key={route.path}
-              path={route.path}
-              component={route.component}
-            />
-          ))}
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <div style={{ width: '100%' }}>
+          <Switch>
+            {routes.map((route) => (
+              <Route
+                key={route.path}
+                path={route.path}
+                component={route.component}
+              />
+            ))}
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </Provider>
   )
 }
 
