37256a1305724cb375fc82bb343ae524a2f58ed0
Sebastian Siejek <contact@sebastiansiejek.dev>
2020-09-25 20:21:21 +0200

    feat: add Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react'
 import store from 'store'
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { Layout } from 'antd'
 import { Provider } from 'react-redux'
 import { routes } from 'routes/configRoutes'
-import { isLogin } from 'store/user'
 
 function App() {
   return (
     <Provider store={store}>
       <BrowserRouter>
-        {isLogin() && <Redirect to="/" />}
-        <Switch>
-          {routes.map((route) => (
-            <Route
-              key={route.path}
-              path={route.path}
-              component={route.component}
-            />
-          ))}
-        </Switch>
+        <Layout style={{ height: '100vh' }}>
+          <Switch>
+            {routes.map((route) => (
+              <Route
+                key={route.path}
+                path={route.path}
+                component={route.component}
+              />
+            ))}
+          </Switch>
+        </Layout>
       </BrowserRouter>
     </Provider>
   )
