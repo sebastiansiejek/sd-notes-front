5298166e1378518175b2a85806e3a18b0e57fcfe
Sebastian Siejek <contact@sebastiansiejek.dev>
2020-09-25 19:54:39 +0200

    feat: add logout action

--- src/store/user.ts
+++ src/store/user.ts
@@ -1,4 +1,5 @@
 import { createSlice, Dispatch } from '@reduxjs/toolkit'
+import { notification } from 'antd'
 
 const slice = createSlice({
   name: 'user',
@@ -10,20 +11,37 @@ const slice = createSlice({
       state.user = action.payload
       localStorage.setItem('user', JSON.stringify(action.payload))
     },
-    logoutSuccess: (state, action) => {
+    logoutSuccess: (state) => {
       state.user = null
+      localStorage.removeItem('user')
     }
   }
 })
 
 export default slice.reducer
 
-const { loginSuccess } = slice.actions
+const { loginSuccess, logoutSuccess } = slice.actions
+
 export const login = (token: string) => async (dispatch: Dispatch) => {
   try {
     dispatch(loginSuccess({ token }))
   } catch (e) {
     return console.error(e.message)
+  }
+}
+
+export const logout = () => async (dispatch: Dispatch) => {
+  if (isLogin()) {
+    dispatch(logoutSuccess())
+    notification['success']({
+      message: 'Logged out'
+    })
+  }
+
+  if (!isLogin()) {
+    notification['error']({
+      message: 'You have not been logged in'
+    })
   }
 }
 
