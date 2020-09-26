1232d9572a4095d3f1095c36cb82d19435f9cf0d
Sebastian Siejek <contact@sebastiansiejek.dev>
2020-09-26 20:17:14 +0200

    feat: add NoteEditor in Modal

--- src/templates/MainTemplate/MainTemplate.tsx
+++ src/templates/MainTemplate/MainTemplate.tsx
@@ -1,6 +1,9 @@
+import AddNote from 'atoms/AddNoteButton'
+import NoteEditor from 'organisms/NoteEditor'
 import React from 'react'
-import { Layout } from 'antd'
 import Sider from 'organisms/Sider'
+import { Layout } from 'antd'
+import { Modal } from 'antd'
 
 const { Header, Content } = Layout
 
@@ -21,6 +24,10 @@ export default ({ children }: any) => (
         }}
       >
         {children}
+        <Modal visible={true} title="Note" footer={[]} width="80%">
+          <NoteEditor onChange={(e: any) => console.log(e)} />
+        </Modal>
+        <AddNote />
       </Content>
     </Layout>
   </Layout>
