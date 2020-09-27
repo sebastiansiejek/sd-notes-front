bb78ff12762e0c0d880572912b5a551153c0bd3f
Sebastian Siejek <contact@sebastiansiejek.dev>
2020-09-27 11:53:15 +0200

    feat: add excerpt util function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,3 +2,9 @@ export const getUserToken = () => {
   const userToken = localStorage.getItem('userToken')
   return userToken && JSON.parse(userToken).token
 }
+
+export const excerpt = (str: string, limit: number) => {
+  return str.length > limit
+    ? str.substr(0, str.lastIndexOf(' ', limit)) + '...'
+    : str
+}
